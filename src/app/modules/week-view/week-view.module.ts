44516f9655f1efcad6df52f3a5c9f7a2eadb9527
Richard Wright <rbwrightjr91@gmail.com>
2017-12-21 13:27:13 -0800

    create week-view module

---
+++ src/app/modules/week-view/week-view.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WeekViewComponent } from './week-view.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [WeekViewComponent]
+})
+export class WeekViewModule { }
