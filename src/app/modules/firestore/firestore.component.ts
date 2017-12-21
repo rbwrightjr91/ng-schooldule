fc9941c4b9943d3583943417fed0e9a532ee7b96
Richard Wright <rbwrightjr91@gmail.com>
2017-12-21 12:08:04 -0800

    track new files

---
+++ src/app/modules/firestore/firestore.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+
+interface Class {
+  class: string;
+  title: string;
+  days: string[];
+  start: number;
+  end: number;
+}
+
+@Component({
+  selector: 'app-firestore',
+  templateUrl: './firestore.component.html',
+  styleUrls: ['./firestore.component.css']
+})
+
+export class FirestoreComponent implements OnInit {
+
+  classCollection: AngularFirestoreCollection<Class>;
+  classes: Observable<Class[]>;
+
+  constructor(private afs: AngularFirestore) { }
+
+  ngOnInit() {
+
+    this.classCollection = this.afs.collection<Class>('Class');
+    this.classes = this.classCollection.valueChanges();
+
+  }
+
+}
