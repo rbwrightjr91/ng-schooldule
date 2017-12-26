5cc39615a0f2f4893786cfb3a0b1b52b9f089ea3
Richard Wright <rbwrightjr91@gmail.com>
2017-12-26 12:02:16 -0800

    change format of time slots to date instead of number

--- src/app/modules/week-view/week-view.component.ts
+++ src/app/modules/week-view/week-view.component.ts
@@ -14,6 +14,7 @@ import { FirestoreService } from './firestore.service';
 export class WeekViewComponent implements OnInit {
 
   days = ['sun', 'mon', 'tues', 'wed', 'thurs', 'fri', 'sat'];
+  timeSlots: Date[] = [];
 
   classData: Class[];
   earliestClass: Class;
@@ -30,23 +31,32 @@ export class WeekViewComponent implements OnInit {
 
   }
 
-  getTimeSlots(): number[] {
+  getTimeSlots(): Date[] {
 
     // get difference in time between earliest start and latest end, with extra hours to pad schedule
     const timeDiffHours = this.latestClass.end.getHours() - this.earliestClass.start.getHours() + 3;
 
     // create amd return an array with size equal to amount of time slots
-    return Array(timeDiffHours).fill(0).map((x, i) => (this.earliestClass.start.getTime() - 3600 * 1000) + (i * 3600 * 1000));
+    this.timeSlots = Array(timeDiffHours).fill(0).map((x, i) => new Date(
+      new Date().setTime(this.earliestClass.start.getTime() - (3600 * 1000) + (i * 3600 * 1000))
+    ));
+
+    return this.timeSlots;
   }
 
-  inSession(time: number, day: string): boolean {
+  inSession(time: Date, day: string): boolean {
 
     let inSession = false;
 
     for (const c of this.classData) {
 
-      if (c.days.includes(day) && c.start.getTime() <= time  && c.end.getTime() >= time) {
-        inSession = true;
+      if (c.days.includes(day)) {
+
+        // check if class in session during the fu
+        if ((c.start.getHours() <= time.getHours()  && c.end.getHours() >= time.getHours()) || this.partOfHour(c)) {
+          inSession = true;
+        }
+
       }
 
     }
@@ -54,7 +64,8 @@ export class WeekViewComponent implements OnInit {
     return inSession;
   }
 
-  quarter(): boolean {
+  partOfHour(c: Class): boolean {
+
     return false;
   }
 
