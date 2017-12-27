7b386b8a5a749bc9d362902673abd1a35b52c700
Richard Wright <rbwrightjr91@gmail.com>
2017-12-27 10:06:44 -0800

    tracking files

---
+++ src/app/modules/ng-schooldule/ng-schooldule.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+
+import { FirestoreService } from './firestore.service';
+import { NgSchoolduleComponent } from './ng-schooldule.component';
+
+
+import { environment } from '../../../environments/environment';
+import { ClassField } from '@angular/compiler/src/output/output_ast';
+export const firebaseConfig = environment.firebaseConfig;
+
+@NgModule({
+  imports: [
+    CommonModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+  ],
+  exports: [
+    NgSchoolduleComponent
+  ],
+  providers: [
+    FirestoreService
+  ],
+  declarations: [
+    NgSchoolduleComponent
+  ]
+})
+export class NgSchoolduleModule { }
