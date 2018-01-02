5a91859c333ed93b7ecc6541a8bce0b90c2e4d6e
Richard Wright <rbwrightjr91@gmail.com>
2018-01-02 14:30:29 -0800

    removing private keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,5 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: 'AIzaSyBJ3OSQNEK663nTF5Xke8TVWTCCJYUldVI',
-    authDomain: 'schooldule.firebaseapp.com',
-    databaseURL: 'https://schooldule.firebaseio.com',
-    projectId: 'schooldule',
-    storageBucket: 'schooldule.appspot.com',
-    messagingSenderId: '780177868534'
-  }
+  firebaseConfig: null
 };
